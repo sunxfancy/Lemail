81026217bdc5b1ca97b20a755740abfdeb7f6003
sxf <sun_xfancy@163.com>
2015-06-29 18:45:03 +0800

    修改了数据库 添加了Mail的测试类

--- src/main/java/lemail/utils/Mail.java
+++ src/main/java/lemail/utils/Mail.java
@@ -2,11 +2,13 @@ package lemail.utils;
 
 import javax.mail.*;
 import javax.mail.internet.*;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
 
 /**
+ * 邮件的基础操作类
  * Created by sxf on 15-6-29.
  */
 public class Mail {
@@ -14,11 +16,16 @@ public class Mail {
     private String username;
     private String password;
     private String hostname;
+    private String emailprovider = "imap";
+    private Authenticator auth;
 
-    public Mail(String username, String password, String hostname) {
-        this.username = username;
-        this.password = password;
-        this.hostname = hostname;
+
+    public Mail(String _username, String _password, String _hostname) {
+        this.username = _username;
+        this.password = _password;
+        this.hostname = _hostname;
+
+        auth = new MyAuthenticator(username,password);
     }
 
     private List<String> mail_list = new LinkedList<String>();
@@ -27,19 +34,100 @@ public class Mail {
 
     }
 
-    public void PostMail(String subject, String content, String from, String... to) {
+    private class MyAuthenticator extends Authenticator {
+        String userName = null;
+        String password = null;
+
+        public MyAuthenticator(String username, String password) {
+            this.userName = username;
+            this.password = password;
+        }
+
+        protected PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication(userName, password);
+        }
+    }
+
+    /**
+     * 发送一封邮件
+     * @param subject 主题
+     * @param content 内容
+     * @param from    发件人
+     * @param to      收件人列表
+     * @throws MessagingException 异常情况
+     */
+    public void PostMail(String subject, String content, String from, String... to)
+            throws MessagingException {
         Properties props = new Properties();
         props.put("mail.smtp.host", hostname);
         props.put("mail.smtp.auth", "true");
 
+        Session session = Session.getDefaultInstance(props, auth);
+        session.setDebug(true);
+        // create a message
+        Message msg = new MimeMessage(session);
 
-        Authenticator auth = new Authenticator() {
-            public PasswordAuthentication getPasswordAuthentication()
-            {
-                return new PasswordAuthentication(username, password);
-            }
-        };
+        // set the from and to address
+        InternetAddress addressFrom = new InternetAddress(from);
+        msg.setFrom(addressFrom);
+
+        InternetAddress[] addressTo = new InternetAddress[to.length];
+        for (int i = 0; i < to.length; i++) {
+            addressTo[i] = new InternetAddress(to[i]);
+        }
+        msg.setRecipients(Message.RecipientType.TO, addressTo);
+
+        // Setting the Subject and Content Type
+        msg.setSubject(subject);
+        msg.setContent(content, "text/plain");
+        msg.saveChanges();
+        Transport transport = session.getTransport("smtp");
+        transport.connect(hostname, username, password);
+        transport.sendMessage(msg, msg.getAllRecipients());
+        transport.close();
     }
+
+
+
+    // 得到所有的邮件
+    public Message[] retrieveAllMailMessage() throws Exception {
+        Session session;
+        Store store;
+        Folder folder;
+        Folder inbox_folder;
+
+        Properties props=System.getProperties();
+        props.setProperty("mail.pop3s.rsetbeforequit","true");
+        props.setProperty("mail.pop3.rsetbeforequit","true");
+        session=Session.getInstance(props,null);
+        // 打印出错误信息
+        session.setDebug(true);
+
+        store=session.getStore(emailprovider);
+        store.connect(hostname, username, password);
+        folder=store.getFolder("INBOX");
+        if( folder==null )
+            throw new Exception("No default folder");
+        inbox_folder = folder;
+        inbox_folder.open(Folder.READ_WRITE);
+
+        Message[] msgs = inbox_folder.getMessages();
+
+        for (Message msg : msgs) {
+            System.out.println("################################");
+            if (msg != null) {
+                System.out.println("邮件标题:" + msg.getSubject());
+                System.out.println("邮件发送者：" + Arrays.toString(msg.getFrom()));
+                System.out.println("邮件发送时间：" + msg.getSentDate());
+                System.out.println("邮件正文:"
+                        + ((msg.getContent() == null) ? "没有正文"
+                        : msg.getContent()));
+            }
+        }
+
+        return msgs;
+    }
+
 
 
 }
---
+++ src/test/java/lemail/utils/MailTest.java
@@ -0,0 +1,16 @@
+package lemail.utils;
+
+import junit.framework.TestCase;
+
+public class MailTest extends TestCase {
+
+    public void testPostMail() throws Exception {
+        Mail mail = new Mail("lemailtest@sina.com", "1234qwer", "smtp.sina.com");
+        mail.PostMail("测试一下", "测试邮件啊。。。。", "lemailtest@sina.com", "sun_xfancy@163.com");
+    }
+
+    public void testRetrieveAllMailMessage() throws Exception {
+        Mail mail = new Mail("lemailtest@sina.com", "1234qwer", "imap.sina.com");
+        mail.retrieveAllMailMessage();
+    }
+}
