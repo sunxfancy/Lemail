d02fa45e43c64c09b577a379f364a7c111c30547
vvliebe@gmail.com <vvliebe@gmail.com>
2015-07-04 19:01:53 +0800

    add login page

--- src/main/webapp/app/controllers/loginController.js
+++ src/main/webapp/app/controllers/loginController.js
@@ -1,7 +1,7 @@
 /**
  * Created by vvliebe on 15-7-4.
  */
-LeMailModule.controller('loginController', ['$scope',function($scope){
+LeMailModule.controller('loginController', ['$scope','$http','$location',function($scope, $http, $location){
     $scope.text = "LEMAIL";
     $scope.userinfo = {
         username: '',
@@ -11,7 +11,26 @@ LeMailModule.controller('loginController', ['$scope',function($scope){
     $scope.submitForm = function(isValid){
         $scope.submitted = true;
         if (isValid){
+             $http({
+                 url: '/api/user/login',
+                 method: 'POST',
+                 params: $scope.userinfo
+             }).success(function(response, status, headers, config){
+                 console.log(response);
+                 //console.log(status);
+                 //console.log(headers);
+                 //console.log(config);
+                 if (response.status == 0){
+                     // login success
+                     //$location.path("/home");
+                 }else if(response.status == 1000){
 
+                 }else if(response.status == 1001){
+
+                 }
+             }).error(function(response, status, headers, config){
+
+             });
         }
     }
 }]);
