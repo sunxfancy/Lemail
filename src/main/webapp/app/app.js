16a876cd275d31e2c0f7bb706736b2bf5c6b0e1b
vvliebe@gmail.com <vvliebe@gmail.com>
2015-07-04 22:12:27 +0800

    for pull2

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -5,7 +5,30 @@
 var LeMailModule = angular.module('LeMailModule', ['ngRoute']);
 
 angular.element(document).ready(function(){
+    $http({
+        url: '/api/user/getuser',
+        method: 'POST',
+        params: $scope.userinfo
+    }).success(function(response, status, headers, config){
+        console.log(response);
+        //console.log(status);
+        //console.log(headers);
+        //console.log(config);
+        if (response.status == 0){
+            // login success
+            var $injector = angular.bootstrap(document, ['LeMailModule']);
+            var $controller = $injector.get('$controller');
+            var LeMailController = $controller('LeMailController');
+            LeMailController.user = data;
+            $templateCache.removeAll();
+        }else if(response.status == 1000){
 
+        }else if(response.status == 1001){
+
+        }
+    }).error(function(response, status, headers, config){
+
+    });
 });
 
 LeMailModule.config(['$routeProvider', function($routeProvider){
