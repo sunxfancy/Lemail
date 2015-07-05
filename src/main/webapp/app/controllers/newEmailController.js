a7b54d1ef851bf19c2d2868eaefaade1aac39c2c
sxf <sun_xfancy@163.com>
2015-07-06 00:02:58 +0800

    添加了新增邮件的页面 已经快被angularjs不让用jQuery给搞吐了
    Signed-off-by: sxf <sun_xfancy@163.com>

---
+++ src/main/webapp/app/controllers/newEmailController.js
@@ -0,0 +1,8 @@
+/**
+ * Created by sxf on 15-7-5.
+ */
+
+LeMailModule.controller('newEmailController',
+    ['$scope','$http','$location',function($scope, $http, $location){
+    $scope.content = '';
+}]);
