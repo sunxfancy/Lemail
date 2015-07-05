72b239a05d83b15ad37a2c58358b67b40266106e
vvliebe@gmail.com <vvliebe@gmail.com>
2015-07-05 12:55:12 +0800

    sidebar event end

---
+++ src/main/webapp/app/controllers/distributeAddController.js
@@ -0,0 +1,8 @@
+/**
+ * Created by vvliebe on 15-7-5.
+ */
+LeMailModule.controller('distributListController',  ['$scope','$http',function($scope, $http){
+    $scope.onPageLoad = function(){
+
+    }
+}]);
--- src/main/webapp/app/controllers/homeController.js
+++ src/main/webapp/app/controllers/homeController.js
@@ -1,18 +1,19 @@
 /**
  * Created by vvliebe on 15-7-3.
  */
-LeMailModule.controller('homeController', ['$scope', function($scope){
+LeMailModule.controller('homeController', ['$scope','$templateCache', function($scope, $templateCache){
     $scope.content_url = "";
 
     $scope.$on('changeMainContent', function(event,data){
-        console.log(data+"aaa");
         $scope.content_url = data;
+        //$templateCache.removeAll();
     });
 
     $scope.onload = function(url){
         // 更新界面
         console.log(url);
         $scope.changeMainContent(url);
+        $templateCache.removeAll();
     };
 
     $scope.changeMainContent = function(url){
